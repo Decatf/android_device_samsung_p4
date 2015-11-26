38b29136e1a75104acee090dc4acdf46eaab5b84
RyanG <decatf@gmail.com>
2015-11-26 14:45:18 -0500

    Update build configs

--- device.mk
+++ device.mk
@@ -1,18 +1,44 @@
-# Inherit device configuration for p4.
-# $(call inherit-product, device/samsung/p4/p4.mk)
-
-# Inherit some common cyanogenmod stuff.
-# $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
-
+# Copyright (C) 2011 The Android Open Source Project
 #
-# Setup device specific product configuration.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-PRODUCT_NAME := p4
-PRODUCT_BRAND := samsung
-PRODUCT_DEVICE := p4
-PRODUCT_MODEL := GT-P7500
-PRODUCT_MANUFACTURER := samsung
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-P7500 BUILD_FINGERPRINT=samsung/GT-P7500/GT-P7500:4.0.4/IMM76D/XXLQ8:user/release-keys PRIVATE_BUILD_DESC="GT-P7500-user 4.0.4 IMM76D XXLQ8 release-keys"
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# Release name and versioning
-PRODUCT_RELEASE_NAME := p4
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ril/cbd:root/sbin/cbd \
+    $(LOCAL_PATH)/gpsconfig.xml:system/etc/gpsconfig.xml
+
+# Cameradata
+PRODUCT_COPY_FILES += \
+    device/samsung/p4-common/camera/cameradata/back_camera_test_pattern.yuv:system/cameradata/back_camera_test_pattern.yuv \
+    device/samsung/p4-common/camera/cameradata/datapattern_420sp.yuv:system/cameradata/datapattern_420sp.yuv \
+    device/samsung/p4-common/camera/cameradata/datapattern_front_420sp.yuv:system/cameradata/datapattern_front_420sp.yuv \
+    device/samsung/p4-common/camera/cameradata/front_camera_test_pattern.yuv:system/cameradata/front_camera_test_pattern.yuv
+
+# Hdmi
+PRODUCT_COPY_FILES += \
+    device/samsung/p4-common/hdmi/dectable1.dat:system/etc/hdmi/dectable1.dat \
+    device/samsung/p4-common/hdmi/dectable.dat:system/etc/hdmi/dectable.dat
+
+# Wifi mfg
+PRODUCT_COPY_FILES += \
+    device/samsung/p4-common/wifi/bcmdhd_mfg.bin:system/etc/wifi/bcmdhd_mfg.bin
+
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
+# RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+  ro.telephony.ril_class=SamsungExynos4RIL
+
+# GPS config
+PRODUCT_PROPERTY_OVERRIDES += \
+  my.gps=novzw
