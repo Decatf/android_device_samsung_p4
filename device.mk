21219238ab0afe64224c6feb3760e20b580245eb
RyanG <decatf@gmail.com>
2015-12-09 13:08:03 -0500

    Get APNs list from sample device

--- device.mk
+++ device.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Get list of APNs
+PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ril/cbd:root/sbin/cbd \
     $(LOCAL_PATH)/gpsconfig.xml:system/etc/gpsconfig.xml
